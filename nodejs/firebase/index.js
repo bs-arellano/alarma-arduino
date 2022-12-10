1c095100b5b09fd4a64541f30c9aedf994bc9de0
K0garash1 <brayanarellano1605@gmail.com>
2022-12-09 21:40:30 -0500

    origin

--- nodejs/firebase/index.js
+++ nodejs/firebase/index.js
@@ -1,18 +1,37 @@
 import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, set, get, child } from "firebase/database";
+import { getDatabase, ref, set, get, child, onChildMoved } from "firebase/database";
+import express from 'express'
+import ejs from 'ejs'
+import path from 'path'
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const firebaseConfig = {
     databaseURL: "https://alarma-esp32cam-default-rtdb.firebaseio.com"
 };
-
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-/*
-set(ref(database, 'esp32-cam/ddasdasd'),{
-    'photo': 'asdasdasd'
-});*/
+const fb = initializeApp(firebaseConfig);
+const database = getDatabase(fb);
 
 
-get(ref(database, 'esp32-cam/')).then(registros=>{
-    console.log(registros.val())
-});
+async function leerBase(){
+    let res = await get(ref(database, 'esp32-cam/'))
+    return res.val()
+}
+
+const app = express()
+const port = 5000
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname,'views'))
+
+app.get('/', function(req,res){
+    leerBase().then(data=>{
+        console.log(Object.keys(data).length)
+        res.render('index', {data: data})
+    })
+})
+
+app.listen(port)
+console.log("Server listening in port",port)
